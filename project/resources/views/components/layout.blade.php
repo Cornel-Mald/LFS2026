269094126df7a66db0db5ba08d53b2468a06cb90
Cornel Wanyoike <cornel@mald.nl>
2026-02-20 11:15:30 +0100

    Some laraveln native active nav styling.

--- project/resources/views/components/layout.blade.php
+++ project/resources/views/components/layout.blade.php
@@ -11,27 +11,26 @@
     @vite('resources/css/app.css')
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<div class="flex flex-col min-h-screen">
-
-    <body class="bg-gray-700 min-h-screen flex flex-col text-gray-100 font-serif">
+<body class="bg-gray-700 min-h-screen flex flex-col text-gray-100 font-serif">
+        <div class="flex flex-col min-h-screen">
         <header class="bg-gray-800 p-4 shadow">
             <div class="container mx-auto flex justify-between items-center">
                 <h1 class="text-xl font-bold">{{ $title }}</h1>
                 <nav class="space-x-4">
-                    <a href="/" class="hover:underline">Welcome</a>
-                    <a href="/about" class="hover:underline">About</a>
-                    <a href="/contact" class="hover:underline">Contact</a>
-                    <a href="/ideas" class="hover:underline">Ideas</a>
+                    <a href="/" class="hover:underline {{ request()->is('/') ? 'nav-active' : 'nav-link' }}">Welcome</a>
+                    <a href="/about" class="hover:underline {{ request()->is('about*') ? 'nav-active' : 'nav-link' }}">About</a>
+                    <a href="/contact" class="hover:underline {{ request()->is('contact*') ? 'nav-active' : 'nav-link' }}">Contact</a>
+                    <a href="/ideas" class="hover:underline {{ request()->is('ideas*') ? 'nav-active' : 'nav-link' }}">Ideas</a>
                 </nav>
             </div>
         </header>
         <main class="flex-grow p-8">
             {{ $slot }}
         </main>
-    </body>
-    <footer class="pt-4 bg-black text-gray-300 text-center mt-12">
-        <p>&copy; 2026 Cornel. All rights reserved.</p>
-    </footer>
-</div>
+        <footer class="pt-4 bg-black text-gray-300 text-center mt-12">
+            <p>&copy; 2026 Cornel. All rights reserved.</p>
+        </footer>
+    </div>
+</body>
 
 </html>
